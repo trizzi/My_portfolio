a907d044abb6abbed338804b3172e51125a78ec3
trizzi <patrickakhamiogu@gmail.com>
2024-04-04 22:43:49 +0100

    wrote snapshots fot the book appointment component

---
+++ src/__tests__/bookappointment.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { ThemeContextProvider } from '../context/ThemeContext';
+
+import BookAppointment from '../components/contact/BookAppointment';
+
+test('renders BookAppointment component with Calendly inline widget', () => {
+	// Render the BookAppointment component
+	const { getByTestId } = render(
+		<ThemeContextProvider>
+			<BookAppointment />
+		</ThemeContextProvider>,
+	);
+
+	// Find the Calendly inline widget by its test ID
+	const calendlyWidget = getByTestId('calendly-widget');
+
+	// Assert that the Calendly inline widget is present
+	expect(calendlyWidget).toBeInTheDocument();
+});
--- src/components/contact/BookAppointment.js
+++ src/components/contact/BookAppointment.js
@@ -3,7 +3,9 @@ import { InlineWidget } from 'react-calendly';
 
 const BookAppointment = () => {
 	return (
-		<div className='bg-black h-[100vh]'>
+		<div
+			className='bg-black h-[100vh]'
+			data-testid='calendly-widget'>
 			<InlineWidget url='https://calendly.com/patrickakhamiogu' />
 		</div>
 	);
